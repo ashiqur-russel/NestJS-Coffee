c9af4078aa42e4ce867375f9f268ccc50a8d6fc1
ashiqurras <ashiqurrussel2@gmail.com>
2023-03-17 01:31:54 +0100

    add @index( decorator to help speed up search mechanism)

--- src/events/entities/event.entity/event.entity.ts
+++ src/events/entities/event.entity/event.entity.ts
@@ -1,10 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * To help speed up this search, we can define an index on the “name” column
+ * using the @Index decorator.
+ *  Also Composite index that contains Multiple columns
+ */
+
+@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
   type: string;
+
+  @Index()
   @Column()
   name: string;
   @Column('json')
